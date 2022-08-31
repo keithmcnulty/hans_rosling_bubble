27c14bcf7209c08db846f35b260b584bf063bac5
Keith McNulty <keith.mcnulty@gmail.com>
2022-08-31 07:57:15 +0000

    Update code

--- rosling.R
+++ rosling.R
@@ -1,26 +1,24 @@
 # libraries needed
 
-library(tidyverse)
+library(dplyr)
 library(ggplot2)
 library(viridis)
 library(gganimate)
 library(wbstats)
 
 # rosling chart in one command
-
+rosling <-
 # pull the country data down from the World Bank - three indicators
-wbstats::wb(indicator = c("SP.DYN.LE00.IN", "NY.GDP.PCAP.CD", "SP.POP.TOTL"), 
-                       country = "countries_only", startdate = 1960, enddate = 2018)  %>% 
+wbstats::wb_data(indicator = c("SP.DYN.LE00.IN", "NY.GDP.PCAP.CD", "SP.POP.TOTL"),
+                       country = "countries_only", start_date = 1960, end_date = 2020)  |>
   # pull down mapping of countries to regions and join
-  dplyr::left_join(wbstats::wbcountries() %>% 
-                     dplyr::select(iso3c, region)) %>% 
-  # spread the three indicators
-  tidyr::pivot_wider(id_cols = c("date", "country", "region"), names_from = indicator, values_from = value) %>% 
+  dplyr::left_join(wbstats::wb_countries()  |>
+                     dplyr::select(iso3c, region)) |>
   # plot the data
-  ggplot2::ggplot(aes(x = log(`GDP per capita (current US$)`), y = `Life expectancy at birth, total (years)`,
-                      size = `Population, total`)) +
+  ggplot2::ggplot(aes(x = log(NY.GDP.PCAP.CD), y = SP.DYN.LE00.IN,
+                      size = SP.POP.TOTL)) +
   ggplot2::geom_point(alpha = 0.5, aes(color = region)) +
-  ggplot2::scale_size(range = c(.1, 16), guide = FALSE) +
+  ggplot2::scale_size(range = c(.1, 16), guide = "none") +
   ggplot2::scale_x_continuous(limits = c(2.5, 12.5)) +
   ggplot2::scale_y_continuous(limits = c(30, 90)) +
   viridis::scale_color_viridis(discrete = TRUE, name = "Region", option = "viridis") +
@@ -31,5 +29,7 @@ wbstats::wb(indicator = c("SP.DYN.LE00.IN", "NY.GDP.PCAP.CD", "SP.POP.TOTL"),
   # animate it over years
   gganimate::transition_states(date, transition_length = 1, state_length = 1) +
   gganimate::ease_aes('cubic-in-out')
-  
+
+# save animation as a gif
+anim_save("rosling.gif", rosling, nframes = 125, height = 600, width = 1000)
 
